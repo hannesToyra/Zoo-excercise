b70cda7df6d973d8bf7f23e9aa0503fbbcb90b30
Hanne <Hanne@HANNES>
2025-09-05 22:57:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+public class Program
+{
+
+    public class Animal
+    {
+        public string? Name { get; set; }
+        //public List<Animal> Prey { get; set; }
+
+        public Animal(string? animalName = null/*, List<Animal> prey*/) 
+        {
+            Name = string.IsNullOrEmpty(animalName) ? $"Unnamed {this.GetType().Name.ToLower()}" : animalName;
+            //Prey = prey.Count > 0 ? prey : new List<Animal>();
+        }
+
+    }
+
+    public class Rat : Animal
+    {
+
+    }
+
+
+
+    public class Cage
+    {
+        public List<Animal> Animals { get; set; }
+
+    }
+
+
+    public class Zoo
+    {
+        public List<Cage> Cages { get; set; }
+
+        public void GetNumberOfCages()
+        {
+            Console.WriteLine(Cages.Count);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Animal test = new Animal();
+        Console.WriteLine(test.Name);
+
+        Rat rat = new Rat();
+        Console.WriteLine(rat.Name);
+    }
+}
