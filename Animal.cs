81bf1b6bb8c5911681870bfcd15526d511811cc6
Hannes <hannes.toyra@gmail.com>
2025-09-14 00:06:12 +0200

    Final solution - Zoo excercise :)

---
+++ Animal.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        public string? Name { get; set; }
+        public List<Animal> Prey = new List<Animal>();
+
+        public Animal(string? animalName = null)
+        {
+            Name = string.IsNullOrWhiteSpace(animalName) ? $"Unnamed {this.GetType().Name.ToLower()}" : animalName;
+        }
+        public void GetPreyInfo()
+        {
+            if (Prey.Count == 0)
+            {
+                Console.WriteLine($"The {this.GetType().Name.ToLower()} has no prey");
+                return;
+            }
+
+            foreach (var preyAnimal in Prey)
+            {
+                Console.WriteLine($"The {this.GetType().Name.ToLower()} likes to eat {preyAnimal.GetType().Name.ToLower()}");
+            }
+        }
+    }
+
+    public class Lion : Animal
+    {
+        public Lion(string? animalName = null) : base(animalName)
+        {
+            Prey.AddRange(new List<Animal> { new Goat(), new Jackal(), new WildCat(), new Kite() });
+        }
+    }
+
+
+    public class Mouse : Animal
+    {
+
+    }
+
+
+    public class Goat : Animal
+    {
+
+    }
+
+    public class Rabbit : Animal
+    {
+        public Rabbit(string? animalName = null) : base(animalName)
+        {
+            Prey.Add(new Mouse());
+        }
+    }
+
+    public class Jackal : Animal
+    {
+        public Jackal(string? animalName = null) : base(animalName)
+        {
+            Prey.AddRange(new List<Animal> { new Goat(), new Rabbit() });
+        }
+    }
+
+    public class Owl : Animal
+    {
+        public Owl(string? animalName = null) : base(animalName)
+        {
+            Prey.Add(new Mouse());
+        }
+    }
+
+
+    public class Snake : Animal
+    {
+        public Snake(string? animalName = null) : base(animalName)
+        {
+            Prey.Add(new Mouse());
+        }
+    }
+
+    public class Kite : Animal
+    {
+        public Kite(string? animalName = null) : base(animalName)
+        {
+            Prey.Add(new Snake());
+        }
+    }
+
+    public class WildCat : Animal
+    {
+        public WildCat(string? animalName = null) : base(animalName)
+        {
+            Prey.AddRange(new List<Animal> { new Rabbit(), new Mouse() });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,141 +1,79 @@
 ﻿
 
 using System.Formats.Asn1;
+using Zoo;
 
 public class Program
 {
-
-    public class Animal
-    {
-        public string? Name { get; set; }
-        public List<Animal> Prey = new List<Animal>();
-
-        public Animal(string? animalName = null)
-        {
-            Name = string.IsNullOrWhiteSpace(animalName) ? $"Unnamed {this.GetType().Name.ToLower()}" : animalName;
-        }
-        public void GetPreyInfo()
-        {
-            if (Prey.Count == 0)
-            {
-                Console.WriteLine($"The {this.GetType().Name.ToLower()} has no prey");
-                return;
-            }
-
-            foreach (var preyAnimal in Prey)
-            {
-                Console.WriteLine($"The {this.GetType().Name.ToLower()} likes to eat {preyAnimal.GetType().Name.ToLower()}");
-            }
-        }
-    }
-
-    public class Lion : Animal
-    {
-        public Lion(string? animalName = null) : base(animalName)
-        {
-            Prey.AddRange(new List<Animal> { new Goat(), new Jackal(), new WildCat(), new Kite()});
-        }
-    }
-
-
-    public class Mouse : Animal
-    {
-
-    }
-
-
-    public class Goat : Animal
-    {
-
-    }
-
-    public class Rabbit : Animal
-    {
-        public Rabbit(string? animalName = null) : base(animalName)
-        {
-            Prey.Add(new Mouse());
-        }
-    }
-
-    public class Jackal : Animal
-    {
-        public Jackal(string? animalName = null) : base(animalName)
-        {
-            Prey.AddRange(new List<Animal> { new Goat(), new Rabbit()});
-        }
-    }
-
-    public class Owl : Animal
-    {
-        public Owl (string? animalName = null) : base(animalName)
-        {
-            Prey.Add(new Mouse());
-        }
-    }
-
-
-    public class Snake : Animal
-    {
-        public Snake (string? animalName = null) : base(animalName)
-        {
-            Prey.Add(new Mouse());
-        }
-    }
-
-    public class Kite : Animal
-    {
-        public Kite(string? animalName = null) : base(animalName)
-        {
-            Prey.Add(new Snake());
-        }
-    }
-
-    public class WildCat : Animal
-    {
-        public WildCat (string? animalName = null) : base(animalName)
-        {
-            Prey.AddRange(new List<Animal> { new Rabbit(), new Mouse()});
-        }
-    }
-
     public class Cage
     {
         public List<Animal> AnimalList = new List<Animal>();
 
-        public void GetAnimalsInsideCage(List<Animal> cagedAnimals)
+        public void GetAnimalsInsideCage()
         {
-            if(cagedAnimals == null || cagedAnimals.Count == 0)
+            if(AnimalList == null || AnimalList.Count == 0)
             {
                 Console.WriteLine("There are currently no animals inside this cage");
                 return;
             }
 
-            foreach(Animal animal in cagedAnimals)
+            foreach(Animal animal in AnimalList)
             {
                 Console.Write($"{animal.Name}");
             }
         }
 
-        public void AddAnimal(Animal animal)
+        public bool IsPredatorOf(Animal predator, Animal potentialPrey)
         {
-            
+            return predator.Prey.Any(prey => prey.GetType() == potentialPrey.GetType());
+        }
 
-            foreach(Animal a in AnimalList)
+        public void CageConflict(Animal predator, Animal prey)
+        {
+            Console.WriteLine("Ooops! Seems like you put predator and prey in the same cage");
+            Console.WriteLine($"{prey.GetType().Name} was eaten by {predator.GetType().Name}");
+        }
+
+        public void AddAnimal(Animal animalToAdd)
+        {
+            List<Animal> animalsToRemove = new List<Animal>();
+
+            foreach(Animal animalInCage in AnimalList)
             {
-                if (animal.Prey.Any(prey => prey.GetType() == a.GetType() || a.GetType() == prey.GetType()))
+                if (IsPredatorOf(animalToAdd, animalInCage))
                 {
-                    Console.WriteLine("Ooops! Seems like you put predator and prey in the same cage");
-                    Console.WriteLine($"{a.GetType().Name} was eaten by {animal.GetType().Name}");
-                    //AnimalList.Remove(a);
-                } else if (animal.Prey.Any(prey => a.GetType() == prey.GetType()))
+                    CageConflict(animalToAdd, animalInCage);
+                    animalsToRemove.Add(animalInCage);
+                } 
+                else if (IsPredatorOf(animalInCage, animalToAdd))
                 {
-                    Console.WriteLine("Ooops! Seems like you put predator and prey in the same cage");
-                    Console.WriteLine($"{animal.GetType().Name} was eaten by {a.GetType().Name}");
+                    CageConflict(animalInCage, animalToAdd);
+                    return;
                 }
             }
-            
-            //lägg till nytt djur i listan
-            AnimalList.Add(animal);
+
+            foreach (Animal eatenAnimal in animalsToRemove) 
+            {
+                AnimalList.Remove(eatenAnimal);
+            }
+
+            AnimalList.Add(animalToAdd);
+        }
+    }
+
+    public class Zoo
+    {
+        public List<Cage> cages = new List<Cage>();
+
+        public void GetNumberOfCages()
+        {
+            if (cages == null || cages.Count == 0)
+            {
+                Console.WriteLine("There are no cages at the zoo");
+                return;
+            }
+
+            Console.WriteLine($"There are currently {cages.Count} at the zoo");
         }
     }
 
@@ -149,18 +87,6 @@ public class Program
 
         Lion leo = new Lion();
 
-        Console.WriteLine(aslan.Name);
-
-        //Console.WriteLine(aslan.Prey[0].Name);
-
-        //cage.AnimalList.Add(aslan);
-
-        //cage.AnimalList.Add(leo);
-
-        Bird larry = new Bird();
-
-        cage.AddAnimal(larry);
-
         cage.AddAnimal(leo);
 
         Mouse mickey = new Mouse();
@@ -169,8 +95,28 @@ public class Program
 
         mickey.GetPreyInfo();
 
-        larry.GetPreyInfo();
-
         leo.GetPreyInfo();
+
+
+        WildCat cat = new WildCat();
+
+        cat.GetPreyInfo();
+
+
+        Cage newCage = new Cage();
+
+        
+
+        //Zoo zoo = new Zoo();
+
+        //zoo.GetNumberOfCages();
+
+        Rabbit rabbit = new Rabbit();
+
+        newCage.AddAnimal(cat);
+
+        newCage.AddAnimal(rabbit);
+        
+        newCage.GetAnimalsInsideCage();
     }
 }
